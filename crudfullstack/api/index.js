867b80db9521c1746bf1716f5e94e30c5c40ce6f
Thaciane de Paula Fernandes <info2020.20203029968@gmail.com>
2025-09-23 01:01:47 -0300

    commit

---
+++ crudfullstack/api/index.js
@@ -0,0 +1,17 @@
+console.time('server-start');
+
+import express from "express"
+import userRoutes from "./routes/users.js"
+import cors from "cors"
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.use("/", userRoutes)
+
+app.listen(8800, () => {
+  console.timeEnd('server-start');
+  console.log('Servidor rodando na porta 8800');
+});
